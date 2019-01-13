e025cb974f530488153fc7ec80002d0ca6f73c8b
Volodymyr <klymenkodp@gmail.com>
2019-01-12 22:32:31 -0500

    Add basic layout and installation section

--- src/pages/Installation.jsx
+++ src/pages/Installation.jsx
@@ -3,9 +3,17 @@ import "./Installation.css";
 
 function Installation() {
   return (
-    <header className="App-header">
-      <p>Installation</p>
-    </header>
+    <div className="app-page">
+      <article className="app-article">
+        <h1>Getting started</h1>
+        <div className="article-content">
+          <p>Learn how to install React Cupertino to your project.</p>
+          <h2>Installation</h2>
+          <p>You can install React Cupertino as an npm package:</p>
+          <pre className="codeblock">npm install react-cupertino</pre>
+        </div>
+      </article>
+    </div>
   );
 }
 
