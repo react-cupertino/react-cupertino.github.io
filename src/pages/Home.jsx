f94a4b4d6105cbaefd986b2f3e39454236001d32
Volodymyr <klymenkodp@gmail.com>
2019-03-01 00:22:04 -0500

    Add elements to the page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { PushButton } from 'react-cupertino';
+import Logo from '../assets/rc-red.svg';
 import "./Home.css";
 
 function Home() {
   return (
-    <header className="App-header">
-      <p>React Cupertino</p>
+    <header className="App-body">
+      <img className="App-logo" src={Logo} alt="React Cupertino logo" />
+      <p className="medium-txt">React Cupertino is a UI Component library inspired by Apple's Design Philosophy.</p>
+      <div className="btn-row">
+        <Link to="/install">
+          <PushButton title="Installation" size="large" color="red"
+            style={{ fontSize: "16px" }} />
+        </Link>
+        <Link to="/components">
+          <PushButton title="Docs" size="large" color="red"
+            style={{ fontSize: "16px" }} />
+        </Link>
+      </div>
     </header>
   );
 }
