0a6aa39ac884d9d2d52e9fff610095598b674860
Volodymyr <klymenkodp@gmail.com>
2019-03-10 20:35:45 -0400

    Add parameter to components route

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/components" component={Components} />
+          <Route path="/components/:componentName" component={Components} />
           <Route path="/install" component={Installation} />
         </Switch>
       </div>
