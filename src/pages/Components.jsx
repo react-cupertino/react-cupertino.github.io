74a6a8bad9d7e4e3085d46327a13c01ca15ff8b2
Volodymyr <klymenkodp@gmail.com>
2019-03-10 20:37:06 -0400

    Add sidebar component + read parameter name

--- src/pages/Components.jsx
+++ src/pages/Components.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import Sidebar from '../components/Sidebar/Sidebar';
 import "./Components.css";
 
-function Components() {
+// removes "-" and capitalizes each word
+function formatTitle(title){
+  title = title.replace(/-/g, ' ');
+  title = title.split(' ')
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(' ');
+  
+  return title;
+}
+
+function Components(props) {
+  let componentName = props.match.params.componentName;
+  componentName = formatTitle(componentName);
   return (
-    <header className="App-header">
-      <p>Components</p>
-    </header>
+    <div className="app-page">
+      <article className="app-article">
+        <h1>{componentName}</h1>
+        <div className="article-content">
+          <p>Documentation goes here...</p>
+        </div>
+      </article>
+      <Sidebar />
+    </div>
   );
 }
 
