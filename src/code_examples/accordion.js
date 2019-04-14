d99bcd94d6ffe649a89a5c9dce63e402b08e51d1
Volodymyr <klymenkodp@gmail.com>
2019-04-13 20:20:25 -0400

    Add code example

---
+++ src/code_examples/accordion.js
@@ -0,0 +1,35 @@
+export default `import React from "react";
+import { Accordion } from "react-cupertino";
+import ReactLogo from '../../assets/react-logo.png';
+import AngularLogo from '../../assets/angular-logo.png';
+import VueLogo from '../../assets/vue-logo.png';
+
+const items = [
+    {
+      id: 1,
+      title: 'React',
+      icon: ReactLogo,
+      description:
+        'React is a JavaScript library for building user interfaces'
+    },
+  
+    {
+      id: 2,
+      title: 'Angular',
+      icon: AngularLogo,
+      description:
+        'Angular is a TypeScript-based open-source web application framework.'
+    },
+  
+    {
+      id: 3,
+      title: 'Vue',
+      icon: VueLogo,
+      description:
+        'Vue (pronounced /vjuː/, like view) is a progressive framework for building user interfaces.'
+    }
+];
+
+function Example() {
+  return <Accordion items={items} />;
+}`;
