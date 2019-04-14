585366e9fc4c0713d29cfa905ba8620398c998a3
Volodymyr <klymenkodp@gmail.com>
2019-04-13 20:20:53 -0400

    Display document based on the component

--- src/pages/Components.jsx
+++ src/pages/Components.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Sidebar from '../components/Sidebar/Sidebar';
+import AccordionDocs from './docs/AccordionDocs';
 import "./Components.css";
 
 // removes "-" and capitalizes each word
-function formatTitle(title){
+function formatTitle(title) {
   title = title.replace(/-/g, ' ');
   title = title.split(' ')
     .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
@@ -12,15 +13,27 @@ function formatTitle(title){
   return title;
 }
 
+function getDocFor(componentName) {
+  console.log(componentName);
+  switch(componentName) {
+    case 'accordion':
+      return <AccordionDocs />;
+    default:
+      return 'Documentation for this component is not available at the moment. Sorry 🙁';
+  }
+}
+
 function Components(props) {
   let componentName = props.match.params.componentName;
+  let doc = getDocFor(componentName);
   componentName = formatTitle(componentName);
+
   return (
     <div className="app-page">
       <article className="app-article">
         <h1>{componentName}</h1>
         <div className="article-content">
-          <p>Documentation goes here...</p>
+          {doc}
         </div>
       </article>
       <Sidebar />
