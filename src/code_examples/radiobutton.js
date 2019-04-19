55bb8fed7ae7b57da3b9f6229c9b90bfdc2ce5ac
Volodymyr <klymenkodp@gmail.com>
2019-04-18 22:03:55 -0400

    Add code example for RadioButton

---
+++ src/code_examples/radiobutton.js
@@ -0,0 +1,12 @@
+export default `import React from "react";
+import { RadioButton } from "react-cupertino";
+
+function Example() {
+  return(
+    <div>
+        <RadioButton id="1" title="Red" color="red" checked />
+        <RadioButton id="2" title="Orange" color="orange" />
+        <RadioButton id="3" title="Green" color="green" />
+    </div>
+  );
+}`;
