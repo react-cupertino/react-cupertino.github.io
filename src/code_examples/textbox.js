2cedd4e6250a1d110ea2ef7721def558878a96c9
Volodymyr <klymenkodp@gmail.com>
2019-04-19 19:54:38 -0400

    Add code example for textbox

---
+++ src/code_examples/textbox.js
@@ -0,0 +1,11 @@
+export default `import React from "react";
+import { Textbox } from "react-cupertino";
+
+function Example() {
+  return(
+    <div>
+        <Textbox label="Login" />
+        <Textbox label="Password" type="password" />
+    </div>
+  );
+}`;
