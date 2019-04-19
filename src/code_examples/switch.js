23cb7294ad33929ef6f0622d56cd78cc3a93237f
Volodymyr <klymenkodp@gmail.com>
2019-04-18 22:19:37 -0400

    Add code example for switch

---
+++ src/code_examples/switch.js
@@ -0,0 +1,10 @@
+export default `import React from "react";
+import { Switch } from "react-cupertino";
+
+function Example() {
+  return(
+    <div>
+        <Switch colorChecked="red" checked />
+    </div>
+  );
+}`;
