b8c35735aa1415960c8fdea1ee43982be7887088
Volodymyr <klymenkodp@gmail.com>
2019-04-18 23:00:41 -0400

    Add code example for slider

---
+++ src/code_examples/slider.js
@@ -0,0 +1,10 @@
+export default `import React from "react";
+import { Slider } from "react-cupertino";
+
+function Example() {
+  return(
+    <div>
+        <Slider backgroundColor="orange" progressColor="green" />
+    </div>
+  );
+}`;
