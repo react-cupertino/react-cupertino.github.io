f8e17137c64e24b4b17350b1d8a712b43ccd51c7
Volodymyr <klymenkodp@gmail.com>
2019-04-19 20:19:28 -0400

    Add code example for push button

---
+++ src/code_examples/pushbutton.js
@@ -0,0 +1,12 @@
+export default `import React from "react";
+import { PushButton } from "react-cupertino";
+
+function Example() {
+  return(
+    <div>
+        <PushButton  color="green" size="small" title="Submit" />
+        <PushButton  color="pink" title="Beautiful button" />
+        <PushButton  size="large" title="Large button" />
+    </div>
+  );
+}`;
