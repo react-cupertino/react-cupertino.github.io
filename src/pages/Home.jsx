9ddffac419fe5241c5cc9ef51df0c39a75199300
Volodymyr <klymenkodp@gmail.com>
2020-03-09 20:51:16 -0400

    update logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { PushButton } from 'react-cupertino';
-import Logo from '../assets/rc-red.svg';
+import { PushButton } from "react-cupertino";
+import Logo from "../assets/logo.svg";
 import "./Home.css";
 
 function Home() {
   return (
     <header className="App-body">
       <img className="App-logo" src={Logo} alt="React Cupertino logo" />
-      <p className="medium-txt">React Cupertino is a UI Component library inspired by Apple's Design Philosophy.</p>
+      <p className="medium-txt">
+        React Cupertino is a UI Component library inspired by Apple's Design
+        Philosophy.
+      </p>
       <div className="btn-container">
         <Link to="/install">
-          <PushButton title="Installation" size="large" color="red"
-            style={{ fontSize: "16px" }} />
+          <PushButton
+            title="Installation"
+            size="large"
+            color="red"
+            style={{ fontSize: "16px" }}
+          />
         </Link>
         <Link to="/components/accordion">
-          <PushButton title="Docs" size="large" color="red"
-            style={{ fontSize: "16px" }} />
+          <PushButton
+            title="Docs"
+            size="large"
+            color="red"
+            style={{ fontSize: "16px" }}
+          />
         </Link>
       </div>
     </header>
